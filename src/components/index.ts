dfe41da542691540860f434f7fa1d6a13ac64cef
pain-arch <himulol123@gmail.com>
2024-10-24 18:58:20 +0600

    project_start

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,13 @@
 import Icons from "./global/icons";
+import Container from "./global/container";
+import Wrapper from "./global/wrapper";
+import Navbar from "./navigation/navbar"; 
+
+
 
 export {
     Icons,
+    Navbar,
+    Container,
+    Wrapper,
 }
