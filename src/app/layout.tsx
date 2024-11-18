441e750461e6530d3e5efae02e2dc307a89bc9eb
pain-arch <himulol123@gmail.com>
2024-11-18 22:05:32 +0600

    Add ClerkProvider to RootLayout and set metadata from SITE_CONFIG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { SITE_CONFIG } from "@/config";
 
 const font = Inter({ subsets: ["latin"] });
+
+export const metadata = SITE_CONFIG;
 
 export default function RootLayout({
     children,
@@ -16,8 +21,11 @@ export default function RootLayout({
                     "min-h-screen bg-background text-foreground antialiased max-w-full overflow-x-hidden",
                     font.className
                 )}
-            >
+            >   
+            <ClerkProvider appearance={{baseTheme:dark}}>
                 {children}
+            </ClerkProvider>
+                
             </body>
         </html>
     );
